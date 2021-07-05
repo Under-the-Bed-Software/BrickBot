e279af2a841cf19e5e9c7af4d0e32bdaff884533
Mike Eisemann <me@michaeleisemann.com>
2021-07-04 23:48:33 -0400

    Initial commit

--- BrickServ3/main.swift
+++ BrickServ3/main.swift
@@ -6,6 +6,47 @@
 //
 
 import Foundation
+import Dispatch
+import IRC
 
-print("Hello, World!")
+let bot = BrickServBot(options: BrickServBot.Options(port: 6697, host: "irc.libera.chat", nickname: IRCNickName("brickbot")!, userInfo: IRCUserInfo(username: "bricks", usermask: IRCUserMode(), realname: "bricks")))
+bot.connect()
 
+let queue = DispatchQueue(label: "REPL")
+
+queue.async {
+    var connected = true
+    bot.ircClient.sendMessage(.init(command: .JOIN(channels: [IRCChannelName("#danshou")!], keys: nil)))
+
+    while(connected) {
+        print("> ", terminator: "")
+        let rawInput = readLine()!
+        var splitInput = rawInput.split(separator: " ")
+        let command = splitInput.removeFirst()
+        switch command {
+        case "PRIVMSG" :
+            print(splitInput)
+            bot.ircClient.sendMessage(.init(command: .PRIVMSG([IRCMessageRecipient("#danshou")!], "HELLO")))
+        case "NICK" :
+            print(splitInput)
+        case "JOIN" :
+            if splitInput.isEmpty {
+                print("not enough arguments")
+                break
+            }
+            bot.ircClient.sendMessage(.init(command: .JOIN(channels: [IRCChannelName(String(splitInput.removeFirst()))!], keys: nil)))
+        case "?" :
+            print("help")
+        default :
+            print("UNKNOWN COMMAND")
+        }
+//        if rawInput == "EXIT" {
+//            connected.toggle()
+//            bot.ircClient.close()
+//        } else {
+//            bot.ircClient.sendMessage(.init(command: .JOIN(channels: [IRCChannelName(rawInput)!], keys: nil)))
+//        }
+    }
+}
+
+dispatchMain()
